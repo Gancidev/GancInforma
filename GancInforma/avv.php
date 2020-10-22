8f22f80e42c932c7fae77ea0b9229273d604bdef
Giuseppe Ganci <73232718+Gancidev@users.noreply.github.com>
2020-10-22 21:02:00 +0200

    WebApp

---
+++ GancInforma/avv.php
@@ -0,0 +1,36 @@
+<html lang="it">
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    Ganci
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+  <!-- CSS Files -->
+  <link href="material-dashboard.css?v=2.1.0" rel="stylesheet" />
+</head>
+<body class="dark-edition">
+    <div class="main-panel">
+      <div class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-8">
+              <div class="card">
+                <div class="card-header card-header-info">
+                  <h4 class="card-title">AVVERTENZE</h4>
+                </div>
+                <div class="card-body">
+                    <h5>I dati presenti in quest'app sono stati forniti dal Dipartimento della Protezione Civile e servono solo come indicazione dell'attuale situazione in Italia. <br><br> Si precisa che non si trarrà alcun profitto da quest'app e che è stata ideata al solo scopo di fornire una chiara e facile visione all'utente che la utilizza.</h5>
+                </div>
+              </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+ </body>
+</html>
